73e61a20671e8a3ae678f6464ccb06b64f4638a1
jbergman <jbergman@localhost>
2016-06-01 19:17:07 -0700

    problem 20

---
+++ p20.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var p []int
+	p = append(p, 1)
+
+	for i := 2; i <= 100; i++ {
+		p = mul(i, p)
+	}
+
+	ans := 0
+	for _, x := range p {
+		ans += x
+	}
+	fmt.Println(ans)
+}
+
+func mul(x int, y []int) []int {
+	var ans []int
+	for offset, digit := range getDigits(x) {
+		for j, c, x := 0, 0, 0; j < len(y) || c != 0; j++ {
+			if j < len(y) {
+				x = y[j]*digit + c
+			} else {
+				x = c
+			}
+			c = x / 10
+			if offset+j < len(ans) {
+				ans[offset+j] += x % 10
+			} else {
+				ans = append(ans, x%10)
+
+			}
+		}
+	}
+	return ans
+}
+
+func getDigits(i int) []int {
+	var digits []int
+	for {
+		digits = append(digits, i%10)
+		if i/10 == 0 {
+			break
+		}
+		i /= 10
+	}
+	return digits
+}
