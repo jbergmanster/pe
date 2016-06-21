d844191b30d0a0cbec3642189ae663c31920e3d4
jbergmanster <jbergmanster@gmail.com>
2016-06-20 21:37:32 -0700

    Problem 34
    Not working yet.

---
+++ p34.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const max = 2540160
+	f := [10]int{0, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880}
+	ans := 0
+	for i := 0; i < max; i++ {
+		sum := 0
+		for j := i; j > 0; j /= 10 {
+			d := j % 10
+			sum += f[d]
+		}
+		if sum == i {
+			fmt.Println(i)
+			ans += sum
+		}
+	}
+	fmt.Println(ans)
+}
